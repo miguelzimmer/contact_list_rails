42e40bfd1701d092d5a46dcfd47f961b1e20ead0
Miguel <miguelzimmerman.leugim@gmail.com>
2021-06-17 09:16:13 -0300

    Paginas Staticas e criação de rotas

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,4 +3,14 @@ class StaticPagesController < ApplicationController
     def index 
         @nome = "Miguel"
     end
+
+    def sobre
+    
+    end
+    
+    def contato
+    
+    end
+    
+
 end
